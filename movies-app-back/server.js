ec8d7911b266b3f5edbb6441aa02f13b4b4b7f5f
Ana Ivanov <ana_ivanovana@yahoo.com>
2019-01-06 13:08:43 +0200

    Backend: enable cors

--- movies-app-back/server.js
+++ movies-app-back/server.js
@@ -8,6 +8,7 @@ app.listen(8081)
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
